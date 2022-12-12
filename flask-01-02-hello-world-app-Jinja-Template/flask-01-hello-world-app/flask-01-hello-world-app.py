38fd0465d0617fca17d7455128419bfeb3ddb112
cagritt <toptascagri@gmail.com>
2022-12-12 23:15:39 +0300

    flask 3-4 added

--- flask-01-02-hello-world-app-Jinja-Template/flask-01-hello-world-app/flask-01-hello-world-app.py
+++ flask-01-02-hello-world-app-Jinja-Template/flask-01-hello-world-app/flask-01-hello-world-app.py
@@ -1,8 +1,8 @@
 from flask import Flask  # flask kütüphanesinden çekiyor
 
-app = Flask(__name__)  # bu 3 komut from, app if flaskın temel komutu
+app = Flask(__name__)  # bu 3 komut -from, app, if- flaskın temel komutu
 
-@app.route('/')    #
+@app.route('/')    #decorator
 
 
 def hello():
@@ -17,9 +17,9 @@ def second():
 def third():
     return 'This is the subpage of third page'   #http://localhost:5000/third/subthird
 
-@app.route('/forth/<string:id>')
+@app.route('/forth/<string:id>')  # numara girince bilgi çekiyor    localhost:5000/forth/sayı  yazınca çıkıyor
 def forth(id):
     return f'Id number of this page is {id}'   # bilgi çekme http://localhost:5000/forth/123 123 yerine ne yazarsak o geliyor
 
-if __name__ == '__main__':
-    app.run(debug=True) #localhost:5000 internete yazınca calıştı
+if __name__ == '__main__':  
+    app.run(debug=True) #localhost:5000 internete yazınca calıştı debug=True, port=2000 yazarsak 2000 portundan internete yazmamız lazım
